bdfeb1bf5517b0744b34a902a9c9e85b6d3dcb0b
munawwar <Munawwar1803@gmail.com>
2022-01-15 02:15:49 +0000

    "commit third"

--- src/components/Ac3.js
+++ src/components/Ac3.js
@@ -3,13 +3,15 @@ import Hz from './Hz'
 
 export default function Ac3(props) {
     return (
+        
         <div className="rightdiv" >
-
+ 
             {props.updater3.map((any) =>
                 <>
 
-
+                  
                     <div className='rightdivmain'>
+                        
                         <img className="rightdivimage" src={any.img} />
                         <div className="Rightcontentdiv">
                             <h1 className='rightheading'>{any.heading}</h1>
@@ -18,6 +20,7 @@ export default function Ac3(props) {
                             <span className='rightspan2' >{any.date1}</span>
                         </div>
                     </div>
+                    <Hz className="hellohr"/>
 
 
 
--- src/components/Bollywood.js
+++ src/components/Bollywood.js
@@ -1,15 +1,52 @@
-import Article from "./Article"
+import Ach1 from "./Ach1"
+import Ach3 from "./Ach3"
 
-function Bollywood(props){
-    return(
+
+function Bollywood(props) {
+    return (
         <>
-    
-       <h1>
-            Bollywood
-        </h1>
-        <Article/>
-       </>
-        
+            
+            <div className="top">
+                <div className="left">
+                <div>
+                <h1 className="latest">
+                   Bollywood
+                </h1>
+                <hr className="topposthr" />
+            </div>
+                    <Ach1 />
+
+                    <div className="arrowdiv">
+                        <div className="downarrow"></div>
+                        <div className="arrow">LOAD MORE</div>
+                    </div>
+
+
+
+
+                </div>
+                <div className="right" >
+
+                    <div>
+                        <h1 className="latest">
+                            Top Posts
+                        </h1>
+                        <hr className="topposthr" />
+                    </div>
+                    <Ach3 />
+                    <div className="advdiv">
+                        <div className="adv" >
+                            Advertistement
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+        </>
+
     )
 }
 export default Bollywood
