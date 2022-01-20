7d6996b4c94a278ddb7b17f1f65ee69e748dbb40
munawwar <Munawwar1803@gmail.com>
2022-01-20 18:24:46 +0000

    "rool over"

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,17 +9,18 @@ function Header(){
        <h3 className="the">The</h3>
         <h1 className="siren">Siren</h1>
        </div>
-       <div className="navstyle">
-        
+      
+        <div style={{"width":"85vw"}}>
+        <div className="navstyle">
        <NavLink className="navitem" to="/" >Home</NavLink>
        <NavLink className="navitem" to="/bollywood" >Bollywood</NavLink>
        <NavLink className="navitem" to="/technology">Technology</NavLink>
        <NavLink className="navitem" to="/hollywood">Hollywood</NavLink>
        <NavLink className="navitem" to="/fitness">Fitness</NavLink>
        <NavLink className="navitem" to="/food">Food</NavLink>
-       
        </div>
        <Hz/>
+       </div>
        
       
       
